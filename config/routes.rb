7a6cabfa95b3c1776b1ca366644ec617d085fbf8
Guillaume Negro <negro.guillaume@gmail.com>
2021-12-10 11:55:36 +0100

    Rails-React params

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
+  
   # API routes
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
-      resources :slots, only: [ :index ]
+      # resources :slots, only: [ :index ]
+      get 'slots/index'
     end
   end
+  root to: 'slots#index'
 end
