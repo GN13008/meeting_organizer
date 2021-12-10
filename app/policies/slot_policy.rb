64486e634e787fdd061e94cb017fa5c5ec50ddb1
Guillaume Negro <negro.guillaume@gmail.com>
2021-12-10 11:38:03 +0100

    order slot

--- app/policies/slot_policy.rb
+++ app/policies/slot_policy.rb
@@ -1,7 +1,7 @@
 class SlotPolicy < ApplicationPolicy
   class Scope < Scope
     def resolve
-      scope.where("nb_agent > 0")
+      scope.where("nb_agent > 0").order(date: :asc)
     end
   end
 end
